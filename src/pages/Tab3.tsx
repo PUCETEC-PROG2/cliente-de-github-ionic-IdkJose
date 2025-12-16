4d147090e21e56fcff045a3e842c4273bb5a09de
José Herrera <jherrerauemee@gmail.com>
2025-12-16 15:45:21 -0500

    Clase v2 16-12-2025

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,14 +1,29 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonViewDidEnter } from '@ionic/react';
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
-import './Tab3.css';
 import { useState } from 'react';
+import { UserInfo } from '../interfaces/UserInfo'
+import './Tab3.css';
+import { getUserInfo } from '../services/GithubService';
 
 const Tab3: React.FC = () => {
 
-  const [userInfo, setUserInfo] = useState({
+  const [userInfo, setUserInfo] = useState<UserInfo>({
     name: "No encontrado",
-
+    login: "no-username",
+    bio:"No se encuentra usuario",
+    avatar_url: "https://i.pinimg.com/474x/1f/9a/2f/1f9a2f75a38d7030ec36ecbf0a618c76.jpg"
   })
+
+  const loadUserInfo = async () => {
+    const response = await getUserInfo();
+    if (response) {
+      setUserInfo(response);
+    }
+  };
+
+  useIonViewDidEnter(() => {
+    loadUserInfo();
+  });
 
   return (
     <IonPage>
@@ -25,12 +40,12 @@ const Tab3: React.FC = () => {
         </IonHeader>
         <div className="card-container">
           <IonCard className="card">
-            <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+            <img alt="Silhouette of mountains" src={userInfo.avatar_url} />
             <IonCardHeader>
-              <IonCardTitle>José Herrera</IonCardTitle>
-              <IonCardSubtitle>José Herrera</IonCardSubtitle>
+              <IonCardTitle>{userInfo.name}</IonCardTitle>
+              <IonCardSubtitle>{userInfo.login}</IonCardSubtitle>
             </IonCardHeader>
-            <IonCardContent>Este es el perfil de José Herrera</IonCardContent>
+            <IonCardContent>{userInfo.bio}</IonCardContent>
           </IonCard>
         </div>
       </IonContent>
